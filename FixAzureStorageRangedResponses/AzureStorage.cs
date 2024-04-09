c8f3c6514233d568dd5a5795b2ae3e499b018d6b
Brett Sheleski <sheleski@fvtc.edu>
2024-04-09 08:36:44 -0500

    Refactor

--- FixAzureStorageRangedResponses/AzureStorage.cs
+++ FixAzureStorageRangedResponses/AzureStorage.cs
@@ -1,4 +1,6 @@
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
 using System.Diagnostics;
 
 namespace FixAzureStorageRangedResponses
@@ -6,6 +8,11 @@ namespace FixAzureStorageRangedResponses
     [TestClass]
     public class AzureStorage
     {
+        // Setting the DefaultServiceVersion to '2013-08-15' enables Azure Storage to properly
+        // handle HTTP Ranged Content request/responses.
+        // See https://stackoverflow.com/questions/17408927/do-http-range-headers-work-with-azure-blob-storage-shared-access-signatures
+
+
         // Get this from Azure Portal
         const string StorageAccountName = "";
 
@@ -18,30 +25,36 @@ namespace FixAzureStorageRangedResponses
         [TestMethod]
         public async Task SetDefaultStorageVersion20130815()
         {
-            // Setting this property enables Azure Storage to properly handle HTTP Ranged Content request/responses.
-            // See https://stackoverflow.com/questions/17408927/do-http-range-headers-work-with-azure-blob-storage-shared-access-signatures
+            // SETUP
+            var client = GetBlobClient();
+            ServiceProperties properties = await client.GetServicePropertiesAsync();
 
-            var credentials = new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(StorageAccountName, StorageAccountKey);
-            var account = new Microsoft.WindowsAzure.Storage.CloudStorageAccount(credentials, true);
-            var client = account.CreateCloudBlobClient();
-            var properties = await client.GetServicePropertiesAsync();
+            // ACT
             properties.DefaultServiceVersion = ServiceVersion20130815;
-            await client.SetServicePropertiesAsync(properties);
 
+            await client.SetServicePropertiesAsync(properties);
         }
 
         [TestMethod]
         public async Task GetDefaultStorageVersion()
         {
-            // Setting this property enables Azure Storage to properly handle HTTP Ranged Content request/responses.
-            // See https://stackoverflow.com/questions/17408927/do-http-range-headers-work-with-azure-blob-storage-shared-access-signatures
+            // SETUP
+            CloudBlobClient client = GetBlobClient();
 
-            var credentials = new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(StorageAccountName, StorageAccountKey);
-            var account = new Microsoft.WindowsAzure.Storage.CloudStorageAccount(credentials, true);
-            var client = account.CreateCloudBlobClient();
-            var properties = await client.GetServicePropertiesAsync();
+            // ACT
+            ServiceProperties properties = await client.GetServicePropertiesAsync();
 
+
+            // VERIFY
             Assert.AreEqual(ServiceVersion20130815, properties.DefaultServiceVersion);
+        }
+
+        private static CloudBlobClient GetBlobClient()
+        {
+            var credentials = new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(StorageAccountName, StorageAccountKey);
+            bool useHttps = true;
+
+            return new CloudStorageAccount(credentials, useHttps).CreateCloudBlobClient();
         }
     }
 }
